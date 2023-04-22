9832283710036bb7dd4ac5eda89c21fdf09267cb
mnbvmnbv2 <ferdinand@ytteborg.com>
2023-04-22 18:39:16 +0200

    move neighbors to problem

--- problem.py
+++ problem.py
@@ -7,3 +7,15 @@ image = Image.open(path)
 image = np.asarray(image)
 image = image.astype(np.int32)
 image_size = image.shape
+
+neighbors_list = [
+    (0, 0),
+    (1, 0),
+    (0, 1),
+    (-1, 0),
+    (0, -1),
+    (1, 1),
+    (-1, 1),
+    (-1, -1),
+    (1, -1),
+]
