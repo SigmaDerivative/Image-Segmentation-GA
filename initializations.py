737b209bc0df3f6247429b0e2bf8e4861596c7ee
mnbvmnbv2 <ferdinand@ytteborg.com>
2023-05-02 07:30:56 +0200

    fix in inits

--- initializations.py
+++ initializations.py
@@ -1,8 +1,14 @@
 import numpy as np
-from sklearn.cluster import KMeans
+
 
 import problem
-from trees import divide_into_trees, create_connected_array_with_arcs, decouple_array
+from trees import (
+    divide_into_trees,
+    create_connected_array_with_arcs,
+    decouple_array,
+    generate_k_meaned_segmentation,
+    segmentation_to_trees,
+)
 
 
 def generate_random_genome() -> np.ndarray:
@@ -34,39 +40,14 @@ def generate_random_genome_from_MST() -> np.ndarray:
     return genome.flatten()
 
 
-def generate_k_meaned_segmentation(flat: bool = False) -> np.ndarray:
-    """Generate a segmentation from k means.
-
-    Args:
-        flat (bool, optional): Whether to return a flat segmentation or not. Defaults to False.
-
-    Returns:
-        np.ndarray: segmentation.
-    """
-    # randomize how many clusters
-    n_clusters = np.random.randint(3, 20)
-    # generate clusters with k nearest neighbors
-    # settings for faster runtime
-    neigh = KMeans(n_clusters=n_clusters, n_init=3, max_iter=40).fit(
-        problem.image.reshape(-1, 3)
-    )
-    # get cluster labels
-    cluster_labels = neigh.labels_
-    if not flat:
-        segmentation = cluster_labels.reshape(
-            problem.image_shape[0], problem.image_shape[1]
-        )
-    return segmentation
-
-
 def generate_mst_genome(flat: bool = True) -> np.ndarray:
     image_height, image_width, _ = problem.image_shape
 
-    pixel_nodes = divide_into_trees(np.random.randint(4, 8))
+    pixel_nodes = divide_into_trees(np.random.randint(3, 8))
 
     genome = np.array(
         [
-            pixel_nodes[(row, col)].neighbour_direction
+            pixel_nodes[(row, col)].neighbor_direction
             for row in range(image_height)
             for col in range(image_width)
         ]
@@ -76,16 +57,33 @@ def generate_mst_genome(flat: bool = True) -> np.ndarray:
     return genome
 
 
+def generate_clustered_genome(flat: bool = True) -> np.ndarray:
+    image_height, image_width, _ = problem.image_shape
+
+    segmentation = generate_k_meaned_segmentation()
+    plot_type_3(segmentation)
+    tree_split_image = segmentation_to_trees(segmentation)
+
+    genome = np.array(
+        [pixel_node.neighbor_direction for pixel_node in tree_split_image.values()]
+    ).astype(np.uint8)
+    if not flat:
+        genome = genome.reshape(image_height, image_width)
+    return genome
+
+
 if __name__ == "__main__":
     import time
     from tqdm import tqdm
-    from visualization import plot_type_2
+    from visualization import plot_type_2, plot_type_3, plot_type_1
     from segmentation import calculate_segmentation
 
     before = time.time()
-    genome = generate_mst_genome()
+    # genome = generate_mst_genome()
+    gen = generate_clustered_genome()
+    # print([a for a in gen])
     print(f"{time.time() - before} seconds")
 
-    seg = calculate_segmentation(genome)
+    seg = calculate_segmentation(gen)
 
     plot_type_2(seg)
