a57f039a1857e0ceed32f23c05058e699c06450b
mnbvmnbv2 <ferdinand@ytteborg.com>
2023-05-02 02:30:34 +0200

    update pymoo test with color mst generation

--- pymoo_test.py
+++ pymoo_test.py
@@ -1,24 +1,31 @@
 import numpy as np
+from tqdm import tqdm
 from pymoo.core.problem import Problem
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.operators.crossover.sbx import SBX
 from pymoo.operators.mutation.pm import PM
+from pymoo.core.sampling import Sampling
 from pymoo.operators.sampling.rnd import FloatRandomSampling, IntegerRandomSampling
 from pymoo.optimize import minimize
 
-from initializations import (
-    generate_k_meaned_segmentation_flat,
-    generate_random_genome_from_MST,
-)
-from evaluations import evaluate_population, evaluate_population_from_segments
+from initializations import generate_mst_genome
+from evaluations import evaluate_population
 from segmentation import calculate_segmentation
 from visualization import plot_type_2
 import problem
 
 
+class MSTSampling(Sampling):
+    def _do(self, problem, n_samples, **kwargs):
+        X = np.full((n_samples, problem.n_var), 0, dtype=int)
+        for i in tqdm(range(n_samples)):
+            X[i, :] = generate_mst_genome(flat=True)
+        return X
+
+
 class MyProblem(Problem):
     def _evaluate(self, x, out, *args, **kwargs):
-        x = np.reshape(x, (-1, problem.image_size[0], problem.image_size[1]))
+        x = np.reshape(x, (-1, problem.image_shape[0], problem.image_shape[1]))
         f1, f2, f3 = evaluate_population(x)
         # f1, f2, f3 = evaluate_population_from_segments(x)
         n_f1 = np.negative(f1)
@@ -28,17 +35,18 @@ class MyProblem(Problem):
 
 
 def main():
-    n_vars = problem.image_size[0] * problem.image_size[1]
-    pymoo_problem = MyProblem(n_var=n_vars, n_obj=3, xl=1, xu=20, vtype=int)
+    n_vars = problem.image_shape[0] * problem.image_shape[1]
+    pymoo_problem = MyProblem(n_var=n_vars, n_obj=3, xl=1, xu=4, vtype=int)
     algorithm = NSGA2(
-        pop_size=100,
-        sampling=generate_random_genome_from_MST(),
-        n_offsprings=40,
+        pop_size=20,
+        # sampling=generate_k_meaned_segmentation_flat(),
+        sampling=MSTSampling(),
+        n_offsprings=10,
         crossover=SBX(prob=0.9, eta=15),
         mutation=PM(eta=20),
         eliminate_duplicates=True,
     )
-    stop_criteria = ("n_gen", 2)
+    stop_criteria = ("n_gen", 10)
     results = minimize(
         problem=pymoo_problem,
         algorithm=algorithm,
@@ -48,7 +56,9 @@ def main():
     print(results.X)
     print(results.F)
     for _, res_x in enumerate(results.X):
-        genome = res_x.reshape(problem.image_size[0], problem.image_size[1]).astype(int)
+        genome = res_x.reshape(problem.image_shape[0], problem.image_shape[1]).astype(
+            int
+        )
         segmentation = calculate_segmentation(genome)
         # visualize the segments
         # plot_type_2(genome)
